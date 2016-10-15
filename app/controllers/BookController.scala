253229be398cad1b7fa278d44a8dbeb148977f10
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2016-10-15 18:47:59 +0200

    Close #1: Initial project structure

---
+++ app/controllers/BookController.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.mvc.{Action, Controller}
+
+import scalaj.http.Http
+
+@Singleton
+class BookController @Inject() extends Controller {
+
+  def index() = Action {
+    Ok(views.html.index("Welcome to books' search engine... "))
+  }
+
+  def getBook(isbn: String) = Action {
+
+    // 978-86-88003-71-1
+    // ID dobij ljepse
+    val response = Http("http://www.vbs.rs/scripts/cobiss?ukaz=GETID").asString.body
+    val start = response.indexOf("http://www.vbs.rs/scripts/cobiss?ukaz=SFRM&amp;id=") +
+      "http://www.vbs.rs/scripts/cobiss?ukaz=SFRM&amp;id=".length
+    val id = response.substring(start, start + 16)
+
+
+    // Parametre ljepse
+    val params = Seq("ukaz" -> "SEAR", "ID" -> id, "keysbm" -> "srcfrm", "PF1" -> "BN", "SS1" -> isbn,
+      "OP1" -> "AND",  "PF2" -> "TI", "SS2" -> "", "OP2" -> "AND", "PF3" -> "PY", "SS3" -> "", "OP3" -> "AND",
+      "PF4" -> "KW", "SS4" -> "", "lan" -> "", "mat" -> "51", "scpt" -> "")
+
+    // Html Parse to Book JSON
+    val book = Http("http://www.vbs.rs/scripts/cobiss?id=" + id).postForm(params).asString
+
+    Ok(book.body)
+  }
+
+}
